84f266f6e109921f37893235e343190afaf3800e
Joachim <muchaiminjoachim@gmail.com>
2025-05-05 20:18:50 +0700

    LOL

--- test.c
+++ test.c
@@ -4,10 +4,13 @@
 int main(){
     printf("Hello World");
     printf("CHRSJOOOOOOO BIJIJIIIIIIIIIIIIIIIIIII");
+    printf("idk mennn");
     return 0;
 }
 
 JOY BIJI
 
 Kmu yang BIJIJIIIII
+
+KAMU YANG BIJIII
 
